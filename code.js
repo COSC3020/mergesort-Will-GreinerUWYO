a54165678eeb3bd8a3b799a29191254b75078d5e
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-15 22:37:25 -0600

    Update code.js

--- code.js
+++ code.js
@@ -11,6 +11,14 @@ function merge(left,right) {
             j++;
         }
     }
+    while (i < left.length) {
+        mergedArray.push(left[i]);
+        i++;
+    }
+    while (j < right.length) {
+        mergedArray.push(right[j]);
+        j++;
+    }
     return mergedArray;
 }
 
@@ -20,7 +28,7 @@ function mergesort(array) {
     if (arrLength <= 1) {
         return array;
     }
-    for (let i = 1; i <= arrLength; i = (2 * i)) {
+    for (let i = 1; i < arrLength; i = (2 * i)) {
         for (let j = 0; j < arrLength; j += (2 * i)) {
             // Math.min ensures the midpoint and endpoint never leave the bounds of the function
             let currentMid = Math.min(j + i, arrLength);
