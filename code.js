88e49e1e7d7a4db426c3751bab6c598b72842f1f
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-15 22:21:07 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,42 @@
+function merge(left,right) {
+    let mergedArray = [];
+    let i,j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            mergedArray.push(left[i]);
+            i++;
+        }
+        else {
+            mergedArray.push(right[j]);
+            j++;
+        }
+    }
+    return mergedArray;
+}
+
 function mergesort(array) {
+    const arrLength = array.length;
+    //base case, only one element in the array
+    if (arrLength <= 1) {
+        return array;
+    }
+    for (let i = 1; i <= array_length; i = (2 * i)) {
+        for (let j = 0; j < array_length - 1; j += (2 * i)) {
+            // Math.min ensures the midpoint and endpoint never leave the bounds of the function
+            let currentMid = Math.min(j + i, arrLength);
+            let currentEnd = Math.min(j + (2 * i), arrLength);
+
+            let left = array.slice(j, currentMid);
+            let right = array.slice(currentMid, currentEnd);
+
+            let merged = merge(left,right);
+
+            let x = 0;
+            while (x < merged.length) {
+                array[start + x] = merged[x];
+                x++;
+            }
+        }
+    }
     return array;
 }
