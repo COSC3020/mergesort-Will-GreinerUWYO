392fcbcc3f00c944ae53c90f46bb4db4eb8ad000
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-18 15:45:24 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,27 +1,23 @@
-function merge(left,right) {
-    let mergedArray = [];
-    let i = 0, j = 0;
-    while (i < left.length && j < right.length) {
-        if (left[i] < right[j]) {
-            mergedArray.push(left[i]);
-            i++;
-        }
+function merge(array, start, leftEnd, rightEnd) {
+    while (start < leftEnd && leftEnd < rightEnd) {
+        if (array[start] < array[leftEnd]) {
+            start++;
+        }    
         else {
-            mergedArray.push(right[j]);
-            j++;
+            let currentMove = array[leftEnd];
+            let index = leftEnd;
+
+            while (index > start) {
+                array[index] = array[index - 1];
+                index--;
+            }
+            array[start] = currentMove;
+            start++;
+            leftEnd++;
         }
     }
-    while (i < left.length) {
-        mergedArray.push(left[i]);
-        i++;
-    }
-    while (j < right.length) {
-        mergedArray.push(right[j]);
-        j++;
-    }
-    return mergedArray;
+    return array;
 }
-
 function mergesort(array) {
     const arrLength = array.length;
     //base case, only one element in the array
@@ -31,19 +27,11 @@ function mergesort(array) {
     for (let i = 1; i < arrLength; i = (2 * i)) {
         for (let j = 0; j < arrLength; j += (2 * i)) {
             // Math.min ensures the midpoint and endpoint never leave the bounds of the function
-            let currentMid = Math.min(j + i, arrLength);
-            let currentEnd = Math.min(j + (2 * i), arrLength);
-
-            let left = array.slice(j, currentMid);
-            let right = array.slice(currentMid, currentEnd);
-
-            let merged = merge(left,right);
-
-            let x = 0;
-            while (x < merged.length) {
-                array[j + x] = merged[x];
-                x++;
-            }
+            let start = i;
+            let leftEnd = Math.min(j + i, arrLength);
+            let rightEnd = Math.min(j + (2 * i), arrLength);
+            
+            merge(array,start,leftEnd,rightEnd);
         }
     }
     return array;
